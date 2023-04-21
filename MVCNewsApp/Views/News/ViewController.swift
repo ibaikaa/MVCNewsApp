877c575e69c9c55a877a57f81fe8eada4be9b4d8
ibaikaa <ibaikaaaa@gmail.com>
2023-04-22 05:55:23 +0600

    Initial commit

---
+++ MVCNewsApp/Views/News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCNewsApp
+//
+//  Created by ibaikaa on 22/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
